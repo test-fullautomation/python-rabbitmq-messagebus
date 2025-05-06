32fc4e63a1242085d00ae342318fdff733165631
qth2hi <holger.queckenstedt@de.bosch.com>
2025-05-06 16:57:20 +0200

    Initial setup of installation and documentation tool chain

---
+++ RabbitMqMessagebus/RabbitMqMessagebus.py
@@ -0,0 +1,54 @@
+#  Copyright 2020-2025 Robert Bosch GmbH
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#################################################################################
+#
+# File: RabbitMqMessagebus.py
+#
+# This module .......
+#
+# History:
+#
+# 2025-05:
+#    - TODO
+#
+#################################################################################
+
+import os
+import sys
+
+from RabbitMqMessagebus.version import VERSION, VERSION_DATE
+
+class CRabbitMqMessagebus():
+    """
+TODO
+    """
+
+    def getVersion(self) -> str:
+        """
+Returns the version of Eventbus as string.
+        """
+        return VERSION
+    
+    def getVersionDate(self) -> str:
+        """
+Returns the version date of Eventbus as string.
+        """
+        return VERSION_DATE
+
+    def __init__(self) -> None:
+        """
+TODO
+        """
+        pass
+
